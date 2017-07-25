2d81ccae6928981c8be3b1bf6b4d882935063510
Smartio <Smartio@DESKTOP-3M3ES3H>
2017-07-25 09:25:19 +0300

    Signed-off-by: Smartio <Smartio@DESKTOP-3M3ES3H>

--- src/main/java/Interface/MainController.java
+++ src/main/java/Interface/MainController.java
@@ -76,7 +76,7 @@ public class MainController {
 		this.getstatusforlist = new GetStatusForList(this);
 		this.createexcel = new СreateExcel(this);
 	}
-
+//////////////////
 	@FXML
 	public Line line1, line2, line3, line4, line5, line6, line7, line8, line9;
 
